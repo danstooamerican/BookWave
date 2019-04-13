9116aedf6228ac64050cb6c33ff88f3236517e0a
Daniel Augustin <daniel.augustin1@gmx.de>
2019-04-13 22:09:00 +0200

    created custom control library Created curstom MenuButton moved styling to another project

--- AudiobookPlayerStyles/Properties/Resources.Designer.cs
+++ AudiobookPlayerStyles/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Commons.Properties {
+namespace Commons.Styles.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Commons.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Commons.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Commons.Styles.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
