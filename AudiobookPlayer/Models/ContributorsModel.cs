397627b2747deab33c81aa06e82f26f6cb22930d
Daniel Augustin <daniel.augustin1@gmx.de>
2019-04-17 14:10:25 +0200

    #11 Added MetadataModel Added ContributorsModel

---
+++ AudiobookPlayer/Models/ContributorsModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commons.Models
+{
+    public class ContributorsModel
+    {
+
+        #region Public Properties
+
+        private string[] mAuthors;
+
+        public string[] Authors
+        {
+            get { return mAuthors; }
+            set { mAuthors = value; }
+        }
+
+        private string[] mReaders;
+
+        public string[] Readers
+        {
+            get { return mReaders; }
+            set { mReaders = value; }
+        }
+
+        #endregion
+
+    }
+}
---
+++ AudiobookPlayer/Models/MetadataModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commons.Models
+{
+    public abstract class MetadataModel
+    {
+        #region Public Properties
+
+        private string mPath;
+        public string Path
+        {
+            get { return mPath; }
+            set { mPath = value; }
+        }
+
+        private string mName;
+        public string Name
+        {
+            get { return mName; }
+            set { mName = value; }
+        }
+
+        private string mDescription;
+        public string Description
+        {
+            get { return mDescription; }
+            set { mDescription = value; }
+        }
+
+        private ContributorsModel mContributors;
+
+        public ContributorsModel Contributors
+        {
+            get { return mContributors; }
+            set { mContributors = value; }
+        }
+
+        private DateTime mRelease;
+
+        public DateTime Release
+        {
+            get { return mRelease; }
+            set { mRelease = value; }
+        }
+
+
+
+
+        #endregion
+    }
+}
