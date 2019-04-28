aca7ee48c9f0553e096b2ea5ae56aa3d9669c6e2
pavelzw <pavelzw@gmail.com>
2019-04-29 01:02:40 +0200

    implemented title bar animation prototype and scrollbar
    Co-Authored-By: Daniel Augustin <uzhpf@student.kit.edu>

---
+++ AudiobookPlayerStyles/Converter/VerticalOffsetVisibilityNegConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Commons.Styles.Converter
+{
+    public class VerticalOffsetVisibilityNegConverter : IValueConverter
+    {
+        public object Convert(object offset, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (offset != null)
+            {
+                var sharedPageTitle = parameter as TextBlock;
+                if (sharedPageTitle != null)
+                {
+                    double height = double.Parse(sharedPageTitle.Text, CultureInfo.InvariantCulture);
+                    double threshhold = height * 0.6;
+                    double offsetDouble = double.Parse(offset.ToString());
+                    if (offsetDouble > threshhold)
+                    {
+                        return Visibility.Visible;
+                    }
+                }
+
+            }
+            return Visibility.Hidden;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
