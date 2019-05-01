382ef4df292b5f7a6d24f52c9efb41705f1219a9
pavelzw <pavelzw@gmail.com>
2019-05-01 17:31:35 +0200

    added SelectFolder and FolderHandler

---
+++ AudiobookPlayer/Logic/FolderHandler.cs
@@ -0,0 +1,27 @@
+﻿using Commons.Models;
+using GalaSoft.MvvmLight;
+using System.Collections.ObjectModel;
+
+namespace Commons.Logic
+{
+    public class FolderHandler : ObservableObject
+    {
+        private string mFolderPath;
+        public string FolderPath
+        {
+            get { return mFolderPath; }
+            set { Set<string>(() => this.FolderPath, ref mFolderPath, value); }
+        }
+
+        public FolderHandler()
+        {
+            FolderPath = string.Empty;
+        }
+
+        public ObservableCollection<Chapter> AnalyzeFolder()
+        {
+            return null; // TODO
+        }
+
+    }
+}
--- AudiobookPlayer/ViewModel/AddPageViewModel.cs
+++ AudiobookPlayer/ViewModel/AddPageViewModel.cs
@@ -1,11 +1,15 @@
-﻿using Commons.Models;
+﻿using Commons.Logic;
+using Commons.Models;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace Commons.ViewModel
 {
@@ -22,16 +26,25 @@ namespace Commons.ViewModel
             set { mChapters = value; }
         }
 
+        private FolderHandler mFolderHandler;
+        public FolderHandler FolderHandler { get; set; }
+
         #endregion
 
         #region Commands
+        public ICommand SelectFolderCommand { private set; get; }
+
+        public ICommand AnalyzeFolderCommand { private set; get; }
 
         #endregion
 
         #region Constructors
-
+        
         public AddPageViewModel()
         {
+            FolderHandler = new FolderHandler();
+            SelectFolderCommand = new RelayCommand(SelectFolder);
+            AnalyzeFolderCommand = new RelayCommand(AnalyzeFolder);
             Chapters = new ObservableCollection<Chapter>();
 
             Chapter c = new Chapter(null);
@@ -55,6 +68,19 @@ namespace Commons.ViewModel
 
         #region Methods
 
+        public void SelectFolder()
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                FolderHandler.FolderPath = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        public void AnalyzeFolder()
+        {
+            Chapters = FolderHandler.AnalyzeFolder();
+        }
 
         #endregion
 
