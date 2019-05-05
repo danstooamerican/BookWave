02c15e489bb4b9320819564ee9d9dea2434f6bea
danstooamerican <uzhpf@student.kit.edu>
2019-05-06 00:33:50 +0200

    add comments to Contributors

--- AudiobookPlayer/Models/Contributors.cs
+++ AudiobookPlayer/Models/Contributors.cs
@@ -8,12 +8,18 @@ using System.Threading.Tasks;
 
 namespace Commons.Models
 {
+    /// <summary>
+    /// Includes lists of people who contribute to an audiobook.
+    /// </summary>
     public class Contributors : ObservableObject
     {
 
         #region Public Properties
 
         private ObservableCollection<string> mAuthors;
+        /// <summary>
+        /// List of authors.
+        /// </summary>
         public ObservableCollection<string> Authors
         {
             get { return mAuthors; }
@@ -21,12 +27,20 @@ namespace Commons.Models
         }
 
         private ObservableCollection<string> mReaders;
+        /// <summary>
+        /// List of readers.
+        /// </summary>
         public ObservableCollection<string>  Readers
         {
             get { return mReaders; }
             set { Set<ObservableCollection<string>>(() => this.Readers, ref mReaders, value); }
         }
 
+        /// <summary>
+        /// String representation of all authors. If this Property is set
+        /// in the right format (name1, name2, ..., name n) then the current 
+        /// author list is cleared and new elements are added.
+        /// </summary>
         public string AuthorString {
             get
             {
@@ -46,6 +60,10 @@ namespace Commons.Models
 
         #region Constructors
 
+        /// <summary>
+        /// Creates a new Contributors class with empty lists of
+        /// authors and readers.
+        /// </summary>
         public Contributors()
         {
             Authors = new ObservableCollection<string>();
