e0a9ec4da07daadf6a3ecf85cd39b8067970629b
Daniel Augustin <uzhpf@student.kit.edu>
2019-07-02 16:01:28 +0200

    added library overview to browse page

---
+++ BookWave/ViewModel/BrowseViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Commons.Logic;
+using Commons.Models;
+using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commons.ViewModel
+{
+    public class BrowseViewModel : ViewModelBase
+    {
+
+        private Audiobook mSelected;
+        public Audiobook Selected
+        {
+            get { return mSelected; }
+            set { Set<Audiobook>(() => this.Selected, ref mSelected, value); }
+        }
+
+        private ObservableCollection<Audiobook> mAudiobooks;
+        public ObservableCollection<Audiobook> Audiobooks
+        {
+            get { return mAudiobooks; }
+            set { Set<ObservableCollection<Audiobook>>(() => this.Audiobooks, ref mAudiobooks, value); }
+        }
+
+        public void ReloadLibrary()
+        {
+            Audiobooks = new ObservableCollection<Audiobook>();
+
+            foreach (Audiobook audiobook in AudiobookManager.Instance.Audiobooks)
+            {
+                Audiobooks.Add((Audiobook)audiobook.Clone());
+            }
+        }
+
+    }
+}
