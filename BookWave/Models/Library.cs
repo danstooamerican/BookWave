5b238cf1fbcac8b371c0fc8e869337e7f714e725
Daniel Augustin <uzhpf@student.kit.edu>
2019-07-10 12:40:22 +0200

    added library

---
+++ BookWave/Models/Library.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commons.Models
+{
+    public class Library
+    {
+        #region Properties
+
+        private int mId;
+        public int Id
+        {
+            get { return mId; }
+            set { mId = value; }
+        }
+
+        private string mPath;
+        public string Path
+        {
+            get { return mPath; }
+            set { mPath = value; }
+        }
+
+        private string mTitle;
+        public string Title
+        {
+            get { return mTitle; }
+            set { mTitle = value; }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public Library(int id, string title, string path)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Path = path;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void ScanLibrary()
+        {
+
+        }
+
+        public void LoadMetadata()
+        {
+
+        }
+
+        public void SaveMetadata(Audiobook audiobook)
+        {
+
+        }
+
+        #endregion
+
+    }
+}
