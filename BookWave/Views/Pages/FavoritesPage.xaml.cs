c7613deafffe76586fe037afa03b0607eeced2b4
Daniel Augustin <uzhpf@student.kit.edu>
2019-08-30 13:02:43 +0200

    added toast notification prototype

--- BookWave/Views/Pages/FavoritesPage.xaml.cs
+++ BookWave/Views/Pages/FavoritesPage.xaml.cs
@@ -1,7 +1,12 @@
 ﻿using Infralution.Localization.Wpf;
+using System;
 using System.Globalization;
 using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
 
 namespace BookWave.Desktop.Pages
 {
@@ -44,6 +49,36 @@ namespace BookWave.Desktop.Pages
         private void Button_Click_4(object sender, System.Windows.RoutedEventArgs e)
         {
             CultureManager.UICulture = new CultureInfo("de-DE");
+        }
+
+        private void Button_Click_5(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var xml = @"<toast>
+              <visual>
+                <binding template=""ToastText04"">
+                  <text id=""1"">This is Notification Message</text>
+                    <text id=""2"">This is second line Notification</text>
+                    <text id=""3"">This is Third line Notification</text>
+                  </binding>
+                </visual>
+              </toast>";
+            var toastXml = new XmlDocument();
+            toastXml.LoadXml(xml);
+            var toast = new ToastNotification(toastXml);
+            string tag = "ex" + DateTimeOffset.Now;
+            toast.Tag = tag;
+            toast.Group = "BookWave.Exception";
+
+            ToastNotificationManager.CreateToastNotifier("Sample toast").Show(toast);
+
+            Task.Delay(5000).ContinueWith(t =>
+            {
+                Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    ToastNotificationManager.History.Remove(tag, "BookWave.Exception", "Sample toast");
+                }));
+                
+            });
         }
     }
 }
