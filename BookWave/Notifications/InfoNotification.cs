92aa96fe13f277ac824517d2a51b29d5e175307b
Daniel Augustin <uzhpf@student.kit.edu>
2019-09-05 15:53:27 +0200

    small fixes

--- BookWave/Notifications/InfoNotification.cs
+++ BookWave/Notifications/InfoNotification.cs
@@ -15,16 +15,16 @@ namespace BookWave.Desktop.Notifications
 
         public InfoNotification(string message, MessageOptions options) : base(message, options)
         {
-            Message = message;
+            NotificationMessage = message;
         }
 
         public InfoNotification(string message) : base(message, new MessageOptions())
         {
-            Message = message;
+            NotificationMessage = message;
         }
 
         private string _message;
-        public string Message
+        public string NotificationMessage
         {
             get
             {
